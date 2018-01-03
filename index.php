83b87be081c3cf9e923255a7b40baa2b3e805ed4
Pablo Pazos <pablo.pazos@cabolabs.com>
2018-01-03 14:35:51 -0300

    updated title

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-
-
-
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -12,7 +7,7 @@
     
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Blog</title>
+    <title>CaboLabs - experts in health informatics, interoperability and standards - blog</title>
 
     <!-- Bootstrap -->
     <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
@@ -29,7 +24,7 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 
     <!-- Custom styles for this template -->
-    <link href="css/clean-blog.min.css" rel="stylesheet">
+    <link href="<?=$_base_dir;?>/css/clean-blog.min.css" rel="stylesheet">
     
     <style>
       #edit-menu {
